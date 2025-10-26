a8044e9879581632d424ae191867b51057d8c7ce
siddharth70175-alt <siddharth70175@gmail.com>
2025-10-26 21:26:01 +0530

    Add files via upload

---
+++ day26q2.c
@@ -0,0 +1,48 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with stars spaced irregularly as shown.
+
+*/
+#include <stdio.h>
+
+int main() {
+    // First block
+    printf("*\n\n");
+
+    // Second block
+    printf("*\n*\n*\n\n");
+
+    // Third block
+    printf("*\n*\n*\n*\n*\n\n");
+
+    // Fourth block
+    printf("*\n*\n*\n\n");
+
+    // Last single star
+    printf("*\n");
+
+    return 0;
+}
+
